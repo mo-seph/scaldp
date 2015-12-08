98e7fe6abc9aed3bda547f1236e82927d87b4eb9
mo-seph <dave@mo-seph.com>
2015-12-08 12:04:00 +0000

    Added Eclipse and generated files to .gitignore

---
+++ src/test/scala/temp/TempTest.scala
@@ -0,0 +1,9 @@
+package temp
+
+import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpec, FlatSpec}
+
+class LinkHeaderParserTester extends FlatSpec with Matchers {
+  "this test" should "be run" in {
+    2 should be( 2 )
+  }
+}
