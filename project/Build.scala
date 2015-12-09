b8f185204bda1a5dc9515d68cf8eaafcca984830
mo-seph <dave@mo-seph.com>
2015-12-09 18:08:00 +0000

    Updated to banana-rdf 0.8.1, hacked deps to stay with scalaz 7.0.x

--- project/Build.scala
+++ project/Build.scala
@@ -26,6 +26,7 @@ object ApplicationBuild extends Build {
     resolvers += "bblfish-snapshots" at "http://bblfish.net/work/repo/snapshots/",
     resolvers += "bblfish.net repository" at "http://bblfish.net/work/repo/releases/",
     resolvers += "sesame-repo-releases" at "http://maven.ontotext.com/content/repositories/aduna/",
+    resolvers += Resolver.url("inthenow-releases", url("http://dl.bintray.com/inthenow/releases"))(Resolver.ivyStylePatterns),
     libraryDependencies ++= appDependencies,
     //ideaExcludeFolders := Seq(".idea", ".idea_modules"),
     //sourceDirectories in(Compile, TwirlKeys.compileTemplates) := (unmanagedSourceDirectories in Compile).value,
@@ -38,15 +39,22 @@ object ApplicationBuild extends Build {
     }
   )
 
+  unmanagedSourceDirectories in Compile <<= (scalaSource in Compile)(Seq(_))
+
+  unmanagedSourceDirectories in Test <<= (scalaSource in Test)(Seq(_))
 
 
-  val banana = (name: String) => "org.w3" %% name % "0.7.2-SNAPSHOT" excludeAll (ExclusionRule(organization = "org.scala-stm"))
+  val scala_stm = ExclusionRule(organization = "org.scala-stm")
+  val scala_z = ExclusionRule(organization = "org.scalaz")
+  val in_the_now = ExclusionRule(organization = "com.github.inthenow")
+  val banana = (name: String) => "org.w3" %% name % "0.7.2-SNAPSHOT" excludeAll (scala_stm)
   val semargl = (name: String) => "org.semarglproject" % {"semargl-"+name} % "0.6.1"
   //see http://bblfish.net/work/repo
   val playTLS = (name: String) =>  "com.typesafe.play" %% name % "2.3.11-TLS"
   val scalatest = "org.scalatest" %% "scalatest" % "2.2.4" % "test"
 //  val scalaActors = "org.scala-lang" % "scala-actors" % "2.10.2"
 
+  val banana_version = "0.8.1"
 
   /**
    * @see http://repo1.maven.org/maven2/com/typesafe/akka/akka-http-core-experimental_2.10/
@@ -59,13 +67,13 @@ object ApplicationBuild extends Build {
       scalatest
     )
 
-  val appDependencies = Seq("sesame", "jena", "plantain_jvm", "ntriples_jvm").map(banana) ++
+  val appDependencies = //Seq("sesame", "jena", "plantain_jvm", "ntriples_jvm").map(banana) ++
 //  Seq("core",)
   Seq(
     akkaHttpCore,
     "net.rootdev" % "java-rdfa" % "0.4.2-RC2",
     "nu.validator.htmlparser" % "htmlparser" % "1.2.1",
-    "org.scalaz" %% "scalaz-core" % "7.0.1", // from "http://repo.typesafe.com/typesafe/releases/org/scalaz/scalaz-core_2.10.0-M6/7.0.0-M2/scalaz-core_2.10.0-M6-7.0.0-M2.jar"
+    "org.scalaz" %% "scalaz-core" % "7.0.7", // from "http://repo.typesafe.com/typesafe/releases/org/scalaz/scalaz-core_2.10.0-M6/7.0.0-M2/scalaz-core_2.10.0-M6-7.0.0-M2.jar"
     "org.bouncycastle" % "bcprov-jdk15on" % "1.51",
     "org.bouncycastle" % "bcpkix-jdk15on" % "1.51",
     "net.sf.uadetector" % "uadetector-resources" % "2014.01",
@@ -79,7 +87,17 @@ object ApplicationBuild extends Build {
     "com.typesafe.play" %% "play-mailer" % "2.4.1",
     "com.typesafe.scala-logging" %% "scala-logging-slf4j" % "2.1.2" ,
     // https://bitbucket.org/inkytonik/kiama/
-    "com.googlecode.kiama" %% "kiama" % "1.8.0"
+    "com.googlecode.kiama" %% "kiama" % "1.8.0",
+    "org.w3" %% "examples" % "0.7.1",
+    "org.w3" %% "banana-rdf" % banana_version  exclude("org.w3","examples") excludeAll( scala_stm, scala_z ),
+    "org.w3" %% "banana-sesame" % banana_version excludeAll( scala_stm, scala_z ),
+    "org.w3" %% "banana-jena" % banana_version excludeAll( scala_stm, scala_z ),
+    "org.w3" %% "banana-plantain" % banana_version excludeAll( scala_stm, scala_z )
+    //"org.w3" %% "banana-rdf-examples" % banana_version excludeAll( scala_stm )
+    //"org.w3" %% "jena" % banana_version excludeAll( scala_stm ),
+    //"org.w3" %% "plantain_jvm" % banana_version excludeAll( scala_stm, scala_z ),
+    //"org.w3" %% "ntriples_jvm" % banana_version excludeAll( scala_stm, scala_z )
+    
   )
 
 
